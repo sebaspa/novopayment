5220668411e8dfb16b17ecc23e13e5310688b59e
Sebaspa <sebaspa0107@gmail.com>
2023-02-08 17:19:53 -0500

    Crear submenu en el navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,29 @@ export const Navbar = () => {
     <nav className="hidden md:block bg-white w-full px-4 menu shadow-md">
       <ul className="flex gap-4 menu__ul">
         <li className="menu__li">Lista de tarjetas</li>
-        <li className="menu__li">Tranferencias y pagos</li>
+        <li className="menu__li group">
+          Tranferencias y pagos
+          <ul className="submenu group-hover:block">
+            <li className="submenu__li">Entre tarjetas</li>
+            <li className="submenu__li">Cuenta bancaria</li>
+            <li className="submenu__li group/submenu">
+              Tarjetas de crédito
+              <ul className="submenu-right group-hover/submenu:block">
+                <li className="submenu__li">Visa</li>
+                <li className="submenu__li">Master card</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
         <li className="menu__li group">
           Atención al cliente
-          <ul className="submenu hidden group-hover:block">
+          <ul className="submenu group-hover:block">
             <li className="submenu__li">Bloqueos</li>
             <li className="submenu__li">Cambio de contraseña</li>
           </ul>
-          </li>
+        </li>
         <li className="menu__li">Empresas</li>
       </ul>
     </nav>
-  )
-}
+  );
+};
