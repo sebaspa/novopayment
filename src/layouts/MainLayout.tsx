674a625610ec665c5126bf1c969a96035e823eac
Sebaspa <sebaspa0107@gmail.com>
2023-02-08 15:35:49 -0500

    Creacion del componente para contenido principal

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import { Footer, Header } from "../components";
 
 export const MainLayout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <div className="w-full min-h-[550px]">
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,8 @@
+import background from "../assets/images/bg-content.jpg";
+
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="w-full min-h-[550px] bg-repeat-x" style={{ backgroundImage: `url(${background})` }}>
+    </div>
+  );
+};
