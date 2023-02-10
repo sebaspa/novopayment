2e397a0d43c325635585afc38b166f235bc3382f
Sebaspa <sebaspa0107@gmail.com>
2023-02-10 11:16:14 -0500

    Revert "Se crean y validan tipos"
    This reverts commit 3ef1ac8824036404caa245624523984d2093f74b.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-export const converObjectToArray = (objectToTransform: any) => {
+export const converObjectToArray = (objectToTransform:any) => {
   const objArray: any = [];
   Object.keys(objectToTransform).forEach((key: any) =>
     objArray.push({
@@ -7,59 +7,4 @@ export const converObjectToArray = (objectToTransform: any) => {
     })
   );
   return objArray;
-};
-
-export const getOptionName = (key: string | undefined) => {
-  let optionName = "";
-  switch (key) {
-    case "card_list":
-      optionName = "Lista de tarjetas";
-      break;
-
-      case "pays_transfer":
-      optionName = "Transferencias y pagos";
-      break;
-
-      case "support":
-      optionName = "Atención al cliente";
-      break;
-
-      case "enterprises":
-      optionName = "Empresas";
-      break;
-
-      case "between_cards":
-      optionName = "Entre tarjetas";
-      break;
-
-      case "banks":
-      optionName = "Cuenta bancaria";
-      break;
-
-      case "credit_card":
-      optionName = "Tarjetas de crédito";
-      break;
-
-      case "lock":
-      optionName = "Bloqueos";
-      break;
-
-      case "change_pass":
-      optionName = "Cambio de contraseña";
-      break;
-
-      case "visa":
-      optionName = "Visa";
-      break;
-      
-      case "master_card":
-      optionName = "Master card";
-      break;
-
-    default:
-      optionName = "Texto vacío";
-      break;
-  }
-
-  return optionName;
 };
